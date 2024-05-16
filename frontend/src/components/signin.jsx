499716045549b681fcaff219bf83b6a8b8a1d98b
venkat <118790430+Venkatknight2295@users.noreply.github.com>
2024-05-16 11:39:10 +0530

    changes

--- frontend/src/components/signin.jsx
+++ frontend/src/components/signin.jsx
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 import "./signin.css"
-
+const ViewOffIcon = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#ffffff"} fill={"none"} {...props}>
+    <path d="M22 8C22 8 18 14 12 14C6 14 2 8 2 8" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+    <path d="M15 13.5L16.5 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M20 11L22 13" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M2 13L4 11" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M9 13.5L7.5 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+);
+const ViewIcon = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#ffffff"} fill={"none"} {...props}>
+    <path d="M21.544 11.045C21.848 11.4713 22 11.6845 22 12C22 12.3155 21.848 12.5287 21.544 12.955C20.1779 14.8706 16.6892 19 12 19C7.31078 19 3.8221 14.8706 2.45604 12.955C2.15201 12.5287 2 12.3155 2 12C2 11.6845 2.15201 11.4713 2.45604 11.045C3.8221 9.12944 7.31078 5 12 5C16.6892 5 20.1779 9.12944 21.544 11.045Z" stroke="currentColor" strokeWidth="1.5" />
+    <path d="M15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12Z" stroke="currentColor" strokeWidth="1.5" />
+  </svg>
+);
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
@@ -112,7 +126,7 @@ const SignIn = () => {
           cursor: 'pointer' 
         }}
       >
-        {showPassword ? "👁️ Hide" : "👁️ Show"}
+        {showPassword ? <ViewOffIcon />: <ViewIcon />}
       </button>
     </div>
     <p style={{ color: '#ffbdf1', fontSize: '1.2rem', fontFamily: 'Poppinslight', listStyleType: 'none', padding: 0 }}>
