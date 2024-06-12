95b292d1f8451ce6b9139d20d480d35a3da8e433
Parth Nakum <parthnakum021@gmail.com>
2024-06-12 23:27:20 +0530

    Added Signup and Login page

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./Login.css";
 
-const Login = () => {
+function Login() {
   return (
-    <div className="auth-container">
-      <h2>Login</h2>
-      {/* Add your login form here */}
+    <div className="login">
+        <p class='logintospring'>Log in to Spring</p>
+        
+        <form>
+            <p class='EmailOrUsername'> Email or username </p>
+            <input type="text" placeholder="Email or username"/> <br />
+
+            <p> Password </p>
+            <input type="password" placeholder="Password"/> <br />
+
+            <input type={"button"} value={"Log in"}/> <br />
+        </form>
+
+        <p class='or'>---------------Or---------------</p>
+
+        <button class='button LoginWithbutton'> <span> Continue With Google </span></button> <br />
+        <p class='alreadyhaveaccount'> Don't have an account? <Link to="/signup">Sign up here.</Link></p> 
     </div>
   );
-};
+}
 
 export default Login;
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './SignUp.css';
 
-const SignUp = () => {
+function SignUp() {
   return (
-    <div className="auth-container">
-      <h2>Sign Up</h2>
-      {/* Add your signup form here */}
+    <div className="signup">
+      
+      <p class='signuptospring'>Sign up to start <br/>listening</p>
+
+      <form>
+          <p class='EmailAddress'> Email address</p>
+          <input type="text" placeholder="name@domain.com"/> <br />
+
+          <input type={"button"} value={"Sign Up"}/>
+      </form>
+
+      <p class='or'>--------------  Or  --------------</p>
+      
+      <button class='buttonSignUp SignUpWithbutton'> <span> Sign Up With Google </span></button> <br />
+      
+
+      <p class='alreadyhaveaccount'> Already have an account? <Link to="/login">Log in here.</Link></p>
     </div>
   );
-};
+}
 
 export default SignUp;
