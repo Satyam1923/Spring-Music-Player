df2ea1d764ded0b24d594f62511bbac1d88ff8c1
Satyam Rahtor <satyamr1923@gmail.com>
2024-06-14 03:42:16 +0530

    fix frontend json format

--- frontend/src/Utils.js
+++ frontend/src/Utils.js
@@ -10,7 +10,8 @@ export function secIntoMinSec(timeInSeconds) {
 export const fetchSongData = async (songName, setCurrSong) => {
   try {
     const response = await axios.get(`https://spring-music-player-3hyj.vercel.app/search?songs=${encodeURIComponent(songName)}`);
-    setCurrSong(response.data.data.results[0]);
+    const data = await response.json();
+    setCurrSong(data[0]);
   } catch (error) {
     console.error("There was a problem with the fetch operation:", error);
   }
@@ -19,7 +20,8 @@ export const fetchSongData = async (songName, setCurrSong) => {
 export const fetchSonsgByName = async (songName, setSongs, count = 6) => {
   try {
     const response = await axios.get(`https://spring-music-player-3hyj.vercel.app/search?songs=${encodeURIComponent(songName)}`);
-    const songs = response.data.data.results.slice(0, count);
+    const data = await response.json();
+    const songs = data.slice(0, count);
     setSongs(songs);
   } catch (error) {
     console.error("There was a problem with the fetch operation:", error);
@@ -29,7 +31,8 @@ export const fetchSonsgByName = async (songName, setSongs, count = 6) => {
 export const fetchTopSongs = async (setTopSongs, count = 6) => {
   try {
     const response = await axios.get("https://spring-music-player-3hyj.vercel.app/api/search?songs=top songs");
-    const topSongs = response.data.data.results.slice(0, count);
+    const data = await response.json();
+    const topSongs = data.slice(0, count);
     setTopSongs(topSongs);
   } catch (error) {
     console.error("There was a problem with the fetch operation:", error);
