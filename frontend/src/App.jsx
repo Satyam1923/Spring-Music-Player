5f994040c814f684a5e6912968fc44c93464b75e
satyamr1923@gmail.com <satyamr1923@gmail.com>
2024-06-14 15:46:23 +0530

    routing error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,14 +13,14 @@ const App = () => {
   return (
     <Router>
     <Routes>
-      <Route path="search" element={<Search />} />
-      <Route path="" element={<Home />} />
-      <Route path="aboutus" element={<Aboutus />} />
-      <Route path="terms" element={<Terms />} />
-      <Route path="contactus" element={<Contactus />} />
-      <Route path="blog" element={<Blog />} />
-      <Route path="culture" element={<Culture />} />
-      <Route path="login" element={<Login />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/aboutus" element={<Aboutus />} />
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/contactus" element={<Contactus />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/culture" element={<Culture />} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<PagenotFound />} /> 
     </Routes>
   </Router>  
