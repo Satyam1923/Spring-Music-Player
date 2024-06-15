bd313a355914beca3bf0d77e1dc6e08c7df96ac6
satyamr1923@gmail.com <satyamr1923@gmail.com>
2024-06-15 15:25:37 +0530

    more get request in backend

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,7 @@ function checkUndefinedFields(result) {
     };
 }
 
+//search songs
 app.get('/search', async (req, res) => {
     try {
         const { song } = req.query;
@@ -68,6 +69,135 @@ app.get('/search', async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+
+
+//get albums
+app.get('/search/albums', async (req, res) => {
+    const album = req.query.query; 
+    console.log("Album name is: " + album);
+    const apiUrl = `https://saavn.dev/api/search/albums?query=${encodeURIComponent(album)}`;
+    try {
+        const response = await axios.get(apiUrl);
+        if (response.status !== 200) {
+            throw new Error('Failed to fetch data from the external API');
+        } else {
+            const albumsArray = response.data.data.results.map(result => ({
+                id: result.id||'',
+                name: result.name||'',
+                year: result.year||'',
+                type: result.type||'',
+                playCount: result.playCount||'',
+                language: result.language||'',
+                primaryArtists: result.artists.primary.map(artist => ({
+                    id: artist.id||'',
+                    name: artist.name||'',
+                    url: artist.url||'',
+                })),
+                images: result.image.map(image => ({
+                    quality: image.quality||'',
+                    url: image.url||'',
+                })),
+            }));
+            res.status(200).json(albumsArray);
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+//get album by id
+app.get('/search/album',async(req,res)=>{
+    const id= req.query.id;
+    console.log("Song id is "+id);
+    const apiUrl = `https://saavn.dev/api/albums?id=${encodeURIComponent(id)}`;
+    try{
+        const respone = await axios.get(apiUrl);
+        if (respone.status!=200){
+            throw new Error('Failed to fetch data from the external API');
+        }
+        else{
+            res.status(200).json(respone.data);
+        }
+    }catch(error){
+        res.status(500).json({error:'Internal server error'});
+    }
+})
+
+
+//get playlists
+app.get('/search/playlists',async(req,res)=>{
+    const playlist = req.query.query;
+    const apiUrl = `https://saavn.dev/api/search/playlists?query=${encodeURIComponent(playlist)}`;
+    try{
+        const respone = await axios.get(apiUrl);
+        if (respone.status!=200){
+            throw new Error('Failed to fetch data from the external API');
+        }
+        else{
+            res.status(200).json(respone.data);
+        }
+    } catch(error){
+        res.status(500).json({error:'Internal server error'});
+    }
+})
+
+//get playlist by id
+
+app.get('/search/playlist',async(req,res)=>{
+    const id = req.query.id;
+    const apiUrl = `https://saavn.dev/api/playlists?id=${encodeURIComponent(id)}`;
+    try{
+        const respone = await axios.get(apiUrl);
+        if (respone.status!=200){
+            throw new Error('Failed to fetch data from the external API');
+        }
+        else{
+            res.status(200).json(respone.data);
+        }
+    } catch(error){
+        res.status(500).json({error:'Internal server error'});
+    }
+})
+
+//get artists
+app.get('/search/artists',async(req,res)=>{
+    const artist = req.query.query;
+    const apiUrl = `https://saavn.dev/api/search/artists?query=${encodeURIComponent(artist)}`;
+    try{
+        const respone = await axios.get(apiUrl);
+        if (respone.status!=200){
+            throw new Error('Failed to fetch data from the external API');
+        }
+        else{
+            res.status(200).json(respone.data);
+        }
+    } catch(error){
+        res.status(500).json({error:'Internal server error'});
+    }
+})
+
+
+//get artist by id
+app.get('/search/artist',async(req,res)=>{
+    const id = req.query.id;
+    const apiUrl = `https://saavn.dev/api/artists/${encodeURIComponent(id)}`;
+    try{
+        const respone = await axios.get(apiUrl);
+        if (respone.status!=200){
+            throw new Error('Failed to fetch data from the external API');
+        }
+        else{
+            res.status(200).json(respone.data);
+        }
+    } catch(error){
+        res.status(500).json({error:'Internal server error'});
+    }
+})
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
