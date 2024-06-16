07055c6a4e4c6e225c9db98814676143ec27fc08
Satyam Rahtor <satyamr1923@gmail.com>
2024-06-17 01:47:04 +0530

    test3

--- backend/routes/scheduler.js
+++ backend/routes/scheduler.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import admin from "firebase-admin";
-
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -8,9 +7,9 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-export default async function scheduledFunction() {
+async function fetchDataAndStore() {
   try {
-    const response = await axios.get('https://billboard-api2.p.rapidapi.com/artist-100?date=2024-06-15&range=1-10', {
+    const response = await axios.get('https://billboard-api2.p.rapidapi.com/artist-100', {
       headers: {
         'x-rapidapi-key': '8d51528d8emsh4c55c96fb71ae0fp1c6811jsnf5e56f9801fe',
         'x-rapidapi-host': 'billboard-api2.p.rapidapi.com'
@@ -34,3 +33,6 @@ export default async function scheduledFunction() {
     console.error('Error in fetching data', error);
   }
 }
+
+
+export { fetchDataAndStore };
