05bc0e7dcea2039707a2fbaf3d91585f6126a506
Aman <forcodingakp@gmail.com>
2024-06-17 21:24:11 +0530

    Adds Billboard top artist extraction with caching and error handling.

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,7 @@ import getPlaylist from "./routes/getPlaylist.js";
 import getPlaylistById from "./routes/getPlaylistById.js";
 import getArtists from "./routes/getArtists.js";
 import getArtistsById from "./routes/getArtistById.js";
-import cron from "node-cron";
-import axios from "axios";
-import admin from "firebase-admin"
-import{fetchDataAndStore} from "./routes/scheduler.js"
+import getTopArtists from "./routes/getTopArtists.js"
 
 const app = express();
 const PORT = 3030;
@@ -23,6 +20,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Welcome to the music search API");
 });
+
+
 
 //search songs
 app.use("/search", search);
@@ -45,16 +44,10 @@ app.use("/search/artists", getArtists);
 //get artist by id
 app.use("/search/artist", getArtistsById);
 
-app.get('/putData', async (req, res) => {
-  try {
-    await fetchDataAndStore();
-    res.status(200).json({ message: "Data is sent successfully" });
-  } catch (error) {
-    console.error('Error in fetching data', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+//get top artists
+app.use("/top-artists", getTopArtists);
+
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port${PORT}`);
 });
