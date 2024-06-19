a1b064ee2505ed1cfcda7f75addf9e87e7d6a947
Varsani2520 <varsaniranjani2520@gmail.com>
2024-06-19 18:18:50 +0530

    update config

--- frontend/src/components/Auth/firebase.js
+++ frontend/src/components/Auth/firebase.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,8 +11,9 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-// import "dotenv/config";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-
-	preview: {
-		host: true,
-		port: 3000
-	}
+  plugins: [react()],
+  server: {
+    open: true,
+  },
 });
