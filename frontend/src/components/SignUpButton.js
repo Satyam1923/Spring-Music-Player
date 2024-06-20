2ae5dc39f2243a8841a8b15ca91a9cffd23a641a
Arunima Dutta <arunimadutta16@gmail.com>
2024-06-20 22:02:14 +0530

    Create SignUpButton.js

---
+++ frontend/src/components/SignUpButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUpButton.css';
+
+const SignUpButton = () => {
+  return (
+    <button className="sign-up-button">
+      Sign Up
+    </button>
+  );
+};
+
+export default SignUpButton;
