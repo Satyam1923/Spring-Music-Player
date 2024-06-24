5564ec8f28107253e2ec2c48b3ccaff7367926be
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-24 14:07:41 +0530

    minor changes

--- frontend/src/components/Album.jsx
+++ frontend/src/components/Album.jsx
@@ -115,20 +115,7 @@ const Album = () => {
                     <th className="px-4 py-2 text-left">Album</th>
                     <th className="px-4 py-2 text-left">Date Added</th>
                     <th className="px-4 py-2 text-left">
-                      <svg
-                        className="w-4 h-4"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 8v8m4-4H8"
-                        ></path>
-                      </svg>
+                      Duration
                     </th>
                   </tr>
                 </thead>
