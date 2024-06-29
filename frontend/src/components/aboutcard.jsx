3d5d893f25e9a816567f4f68ed0eab9d25ff5bb9
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-06-29 21:38:17 +0530

    Issue#446 fixed.

--- frontend/src/components/aboutcard.jsx
+++ frontend/src/components/aboutcard.jsx
@@ -11,12 +11,7 @@ export const HoverEffect = ({ items, className }) => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
 
   return (
-    <div
-      className={cn(
-        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-10",
-        className
-      )}
-    >
+    <div className={cn("grid grid-cols-1 px-10 py-10", className)}>
       {items.map((item, idx) => (
         <div
           key={idx}
