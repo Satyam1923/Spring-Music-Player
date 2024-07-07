69124e5baa6f35cca0dc7d276cc9807464ae3212
Rohit Yadav <rk4740779@gmail.com>
2024-07-07 10:21:27 +0530

    updated player ui

--- frontend/src/components/MusicPlayer.jsx
+++ frontend/src/components/MusicPlayer.jsx
@@ -1,19 +1,27 @@
-import React, { useEffect, useRef } from "react";
-import { PiPlaylistBold } from "react-icons/pi";
-import { AiFillLike } from "react-icons/ai";
+import { useEffect, useRef } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/src/styles.scss";
 import "./styles.scss";
-import {CirclePlay,CirclePause,Rewind,VolumeX,Volume2,FastForward,Repeat2} from "lucide-react";
+import "./MusicPlayer.css";
+
+import {
+  CirclePlay,
+  CirclePause,
+  Rewind,
+  VolumeX,
+  Volume2,
+  FastForward,
+  Repeat2,
+} from "lucide-react"; 
 
 function MusicPlayer({ currSong, shouldAutoPlay }) {
   const songName = currSong.name || "Reminder";
   const songImage =
-    currSong.img || "https://i.scdn.co/image/ab67616d0000b2734718e2b124f79258be7bc452";
+    currSong.img ||
+    "https://i.scdn.co/image/ab67616d0000b2734718e2b124f79258be7bc452";
   const artistName = currSong.artist || "The Weeknd";
   const audioUrl = currSong.url || "";
   const audioPlayerRef = useRef(null);
-  
 
   useEffect(() => {
     if (audioPlayerRef.current && !shouldAutoPlay) {
@@ -21,49 +29,39 @@ function MusicPlayer({ currSong, shouldAutoPlay }) {
     }
   }, [currSong]);
 
-  const customIcons={
-    play:<CirclePlay />,
-    pause:<CirclePause />,
-    rewind:<Rewind />,
+  const customIcons = {
+    play: <CirclePlay />,
+    pause: <CirclePause />,
+    rewind: <Rewind />,
     forward: <FastForward />,
-    loop:<Repeat2 />,
-    volume:<Volume2 />,
-    volumeMute: <VolumeX />
-  }
+    loop: <Repeat2 />,
+    volume: <Volume2 />,
+    volumeMute: <VolumeX />,
+  };
 
   return (
-    <div className="bg-[#18181D] w-full h-full rounded-lg">
-      <div className="flex flex-col h-full justify-between p-2">
-        {/* Image and like details */}
-        <div className="flex h-full pt-6">
-          <div className="flex justify-center h-full w-[20%]">
-            <AiFillLike className="scale-[1.5] mt-2 hover:cursor-pointer" />
-          </div>
-          {/* Song Image */}
-          <div className="flex flex-col justify-start gap-4 h-full w-[60%]">
-            <img src={songImage} alt="song image" className="rounded-2xl w-full aspect-square" />
-            <div className="flex flex-col gap-1">
-              <h2 className="text-white font-medium text-2xl">{songName}</h2>
-              <h3 className="text-white">{artistName}</h3>
-            </div>
-          </div>
-          <div className="flex justify-center h-full w-[20%]">
-            <PiPlaylistBold className="scale-[1.5] mt-2 hover:cursor-pointer" />
-          </div>
+    <div className="player ">
+      <div className="wrapper">
+        <a target="_blank" href="https://www.instagram.com/correiamendsss/">
+          <img className="playerimg" src={songImage} alt="LogoMusicImage" />
+        </a>
+        <div className="info">
+          <h1>{songName}</h1>
+          <p>{artistName}</p>
         </div>
 
-        {/* Audio Player */}
-        <div className="h-[30%] rounded-lg">
+        <div className="controls">
           <AudioPlayer
             ref={audioPlayerRef}
             autoPlay={false}
-            className="rounded-lg bg-[#83ce89] text-black h-full"
+            className="bg-[#18181D] text-white h-full w-full shadow-none "
             src={audioUrl}
             customIcons={customIcons}
           />
         </div>
       </div>
     </div>
+
   );
 }
 
