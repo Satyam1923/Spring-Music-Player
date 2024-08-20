409ca373695230a56decf6c6e79b7a46c5145f64
satyam rathor <satyamr1923@gmail.com>
2024-08-20 16:08:30 +0530

    fix errors

--- frontend/src/components/Auth/firebase.js
+++ frontend/src/components/Auth/firebase.js
@@ -3,26 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_APP_ID,
-//   databaseURL: import.meta.env.VITE_DATABASE_URL, 
-// };
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBNZqJmtwAqYuE0zrtdSMdgcoXZSoRdMew",
-  authDomain: "opensource-2faad.firebaseapp.com",
-  projectId: "opensource-2faad",
-  storageBucket: "opensource-2faad.appspot.com",
-  messagingSenderId: "1060453831050",
-  appId: "1:1060453831050:web:4e94554bd4ec6e9f37f873",
-  measurementId: "G-QL0FTXFWEH"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL, 
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
